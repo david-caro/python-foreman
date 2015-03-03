20872506a4c48181840fbf6b53fe3fd1fc5f0d8c
Lukas Bednar <lbednar@redhat.com>
2015-03-03 09:26:48 +0100

    Fixed match for resources with special plural names
    * Major refactor of the metaclasses and function generating methods
    * Track unbound methods
    * Forcing required params from function definitions
    * Improved handling of complex api endpoints:
    
        An endpoint like /api/resource1/{id1}/resource2/{id2}/another for the
        methods index will be mapped to:
            Resource1.resource2_another_index(id1, id2, <and any non-url params>)
    
    * Improved the parameter documentation, now all of them state if they are
      required or not
    * Added tests module
    * Added test run to tox config
    * Added requirements and test-requirements files
    
    Change-Id: Ie3e6e0e2660761b0cb1117dfd6ada0d2449c5a41
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

---
+++ tests/test_defs.py
@@ -0,0 +1,52 @@
+import os
+import logging
+from foreman.client import Foreman, Resource, requests
+from mocks import SessionMock
+
+
+class DefsGen(object):
+
+    url = 'foreman.example.com'
+
+    def __init__(self, foreman_version, api_version):
+        self.frmn_ver = foreman_version
+        self.api_ver = api_version
+        self.logger = logging.getLogger(
+            "Foreman-%s-%s" % (self.frmn_ver, self.api_ver)
+        )
+
+    def check_api(self):
+        f = self.generate_api()
+        for value in f.__dict__.itervalues():
+            if not isinstance(value, Resource):
+                continue
+            self.check_resource(value)
+
+    def generate_api(self):
+        requests.Session = SessionMock(self.url, self.frmn_ver)
+        self.logger.info("Generate api")
+        return Foreman(
+            self.url,
+            version=self.frmn_ver,
+            api_version=self.api_ver,
+        )
+
+    def check_resource(self, resource):
+        self.logger.info("Checking resource: %s", resource)
+        # assert not resource._unbound_methods
+
+    def __repr__(self):
+        return "DefsGen(frmn_ver=%s, api_ver=%s)" % (
+            self.frmn_ver,
+            self.api_ver,
+        )
+
+
+def test_apis():
+    for json_file in os.listdir("foreman/definitions"):
+        if json_file.endswith('.json'):
+            version = json_file.strip('.json').split('-')
+            if len(version) != 2:
+                continue
+            test = DefsGen(version[0], version[1].strip('v'))
+            yield test.check_api,
