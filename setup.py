4bedd0fe8be316b7bc413358b97116f7d5e4dd66
David Caro <david@dcaro.es>
2016-10-01 23:11:53 +0200

    packaging: remove python2.6 keyword
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -135,7 +135,6 @@ if __name__ == '__main__':
             'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.4',
         ],
