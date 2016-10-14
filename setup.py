4ea14f0f42d50ccab0b9a0568ff9874e8048a820
David Caro <david@dcaro.es>
2016-10-14 19:32:52 +0200

    packaging: use autosemver though distutils
    * Fix also the missing package (closes #75)
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-from autosemver.packaging import (
-    get_authors,
-    get_current_version,
-    get_changelog,
-)
-
-
 PACKAGE_NAME = 'python-foreman'
 URL = 'https://github.com/david-caro/python-foreman'
 
 
 if __name__ == '__main__':
-    with open('AUTHORS', 'w') as authors_fd:
-        authors_fd.write('\n'.join(get_authors()))
-
-    with open('CHANGELOG', 'w') as changelog_fd:
-        changelog = get_changelog(bugtracker_url=URL + '/issues')
-        try:
-            changelog_fd.write(changelog.decode())
-        except AttributeError:
-            changelog_fd.write(changelog)
-
     setup(
+        autosemver=True,
         install_requires=[
+            'autosemver',
             'requests',
             'six',
         ],
         setup_requires=['autosemver'],
         name=PACKAGE_NAME,
-        version=get_current_version(project_name=PACKAGE_NAME),
         include_package_data=True,
+        packages=['foreman'],
         description=(
             'Simple low-level client library to access the Foreman API'
         ),
         author='David Caro',
         author_email='david@dcaro.es',
         url=URL,
+        bugtracker_url=URL + '/issues/',
         license='GPLv2',
         classifiers=[
             'Intended Audience :: Developers',
