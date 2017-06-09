24b1c964fd0136d6fea918d61b540c3e939b3436
David Caro <david@dcaro.es>
2017-06-09 17:04:26 +0200

    tests: add definition with 'import' function
    * This shows the problem stated in #84
    
    Co-authored-by: Ntr0 <git@ntr0py.de>
    Signed-off-by: David Caro <david@dcaro.es>

--- tests/test_defs.py
+++ tests/test_defs.py
@@ -22,8 +22,8 @@ class HasConflictingMethods(Exception):
         )
 
 
-def check_api(url, foreman_version, api_version):
-    cli = generate_api(url, foreman_version, api_version)
+def check_api(url, foreman_version, api_version, cache_dir):
+    cli = generate_api(url, foreman_version, api_version, cache_dir)
     for value in six.itervalues(cli.__dict__):
         if not isinstance(value, Resource):
             continue
@@ -31,13 +31,14 @@ def check_api(url, foreman_version, api_version):
         check_resource(resource=value)
 
 
-def generate_api(url, foreman_version, api_version):
+def generate_api(url, foreman_version, api_version, cache_dir):
     requests.Session = SessionMock(url, foreman_version)
     print("Generating api")
     return Foreman(
         url,
         version=foreman_version,
         api_version=api_version,
+        cache_dir=cache_dir,
     )
 
 
@@ -53,22 +54,34 @@ def check_resource(resource):
     assert resource._own_methods
 
 
-def api_versions():
+def api_versions_in_dir(defs_dir):
     api_versions = []
-    for json_file in os.listdir("foreman/definitions"):
+    for json_file in os.listdir(defs_dir):
         if json_file.endswith('.json'):
-            version = json_file.strip('.json').split('-')
-            if len(version) != 2:
-                continue
+            version = json_file.strip('.json').rsplit('-', 1)
             api_versions.append(version)
+
+    return api_versions
+
+
+def all_api_versions():
+    defs_dirs = [
+        'foreman/definitions',
+        'tests/fixtures/definitions'
+    ]
+    api_versions = []
+    for dirname in defs_dirs:
+        api_versions.extend(
+            api_versions_in_dir(defs_dir=dirname)
+        )
 
     return api_versions
 
 
 @pytest.mark.parametrize(
     'api_version',
-    api_versions(),
-    ids=[':'.join(ver) for ver in api_versions()],
+    all_api_versions(),
+    ids=[':'.join(ver) for ver in all_api_versions()],
 )
 def test_apis(api_version, capsys):
     try:
@@ -76,6 +89,7 @@ def test_apis(api_version, capsys):
             url=URL,
             foreman_version=api_version[0],
             api_version=api_version[1].strip('v'),
+            cache_dir='tests/fixtures',
         )
     except HasConflictingMethods as error:
         print('Got conflicting methods: %s' % error)
